409062c91bebf02d631dc42a43b5ce6f6948241e
LukNav <navasinskaslukas@gmail.com>
2022-09-27 10:35:53 +0300

    Add project files.

---
+++ WindowsFormsApplication/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WindowsFormsApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            string text = "";
+            Program.MainForm.Invoke(new Action(() =>
+            {
+                text = Program.MainForm.textBox1.Text;
+            }));
+            return text;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(string id)
+        {
+            Program.MainForm.Invoke(new Action(() =>
+            {
+                Program.MainForm.textBox1.Text = id;
+            }));
+            return Ok();
+        }
+    }
+}
