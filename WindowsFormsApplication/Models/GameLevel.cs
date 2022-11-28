d132525bbdfe509e0b2b8275ff9759a0529f360b
LukNav <navasinskaslukas@gmail.com>
2022-11-28 20:34:20 +0200

    Last fix (hopefully)

--- WindowsFormsApplication/Models/GameLevel.cs
+++ WindowsFormsApplication/Models/GameLevel.cs
@@ -10,7 +10,7 @@
 
         public GameLevel(Tower towerType, Pawn pawn1, Pawn pawn2, Pawn pawn3)
         {
-            Level = 0;
+            Level = 1;
             TowerType=towerType;
             Pawn1=pawn1;
             Pawn2=pawn2;
