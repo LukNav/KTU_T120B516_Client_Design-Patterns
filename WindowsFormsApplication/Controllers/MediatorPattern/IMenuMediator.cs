901d9b9793a000d3753695be2c1147f620d25de4
LukNav <navasinskaslukas@gmail.com>
2022-12-05 22:55:02 +0200

    Menu Mediator interface added

---
+++ WindowsFormsApplication/Controllers/MediatorPattern/IMenuMediator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApplication.Models;
+
+namespace WindowsFormsApplication.Controllers.MediatorPattern
+{
+    public interface IMenuMediator
+    {
+        string CreateClient();
+        void SetPlayerAsReady();
+        Game GetGameInfo();
+        void UnregisterPlayer();
+    }
+}
